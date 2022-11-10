beee03361a8c20e80e72b850c913d789dfbbc48a
ek223gi <ek223gi@student.lnu.se>
2022-11-10 16:46:09 +0300

    Two errors are added

--- src/main/kotlin/ErrorType.kt
+++ src/main/kotlin/ErrorType.kt
@@ -1,6 +1,7 @@
 enum class ErrorType(var errorName : String, var errorDef : String, var solution : String){
 
     ARRAY_INDEX_OUT_OF_BOUNDS("ArrayIndexOutOfBoundsException", "The given index is higher than the size of the array", "Increase size of the array"),
+    INDEX_OUT_OF_BOUNDS("IndexOutOfBoundsException","The index you are trying to pull does not exist","Increase the loop iteration or decrease the index you are trying to pull"),
+    ILLEGAL_ARGUMENT("IllegalArgumentException","A condition about a variable was disregarded","Correct the variable according to the conditions it has"),
     UNDEFINED("undefined", "undefined", "undefined")
 }
-
