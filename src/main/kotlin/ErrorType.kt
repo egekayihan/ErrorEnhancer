14618b77335c51385961c2f4ccaba9ea591bba3c
ek223gi <ek223gi@student.lnu.se>
2023-02-15 14:24:57 +0100

    Error descriptions and solving methods were updated

--- src/main/kotlin/ErrorType.kt
+++ src/main/kotlin/ErrorType.kt
@@ -1,7 +1,7 @@
 enum class ErrorType(var errorName : String, var errorDef : String, var solution : String){
 
-   // ARRAY_INDEX_OUT_OF_BOUNDS("ArrayIndexOutOfBoundsException", "The given index is higher than the size of the array", "Increase size of the array"),
-    INDEX_OUT_OF_BOUNDS("IndexOutOfBoundsException","The index you are trying to pull does not exist","Increase the loop iteration or decrease the index you are trying to pull"),
-    //ILLEGAL_ARGUMENT("IllegalArgumentException","A condition about a variable was disregarded","Correct the variable according to the conditions it has"),
+    //ARRAY_INDEX_OUT_OF_BOUNDS("ArrayIndexOutOfBoundsException", "Program attempts to access an invalid index in an array", "The bounds of the array should be checked before accessing its elements"),
+    //INDEX_OUT_OF_BOUNDS("IndexOutOfBoundsException","An index of some sort (such as to an array, to a string, or to a vector) is out of range","Check the index you are trying to pull"),
+    ILLEGAL_ARGUMENT("IllegalArgumentException","A method has been passed an illegal or inappropriate argument or wrong number of arguments","Correct the argument for the method it will be used in"),
     UNDEFINED("undefined", "undefined", "undefined")
 }
